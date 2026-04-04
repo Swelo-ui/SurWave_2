e6bec57e8ad7dedb26cf359f2c21c6b3aa14b503
sasha-melech <161236235+sasha-melech@users.noreply.github.com>
2026-04-04 12:24:41 -0400

    feat(varispeed mode): add setting to link pitch and speed shift (#3301)
    * add varispeed ui element
    
    * add varispeed menu functionality
    
    * move speed string out of upstream file
    
    * show speed adjuster when in room
    
    * remove unused variable declaration

--- app/src/main/kotlin/com/metrolist/music/constants/PreferenceKeys.kt
+++ app/src/main/kotlin/com/metrolist/music/constants/PreferenceKeys.kt
@@ -117,6 +117,8 @@ enum class AudioQuality {
 
 val AudioOffload = booleanPreferencesKey("enableOffload")
 
+val VarispeedKey = booleanPreferencesKey("varispeed")
+
 val PersistentQueueKey = booleanPreferencesKey("persistentQueue")
 val PersistentShuffleAcrossQueuesKey = booleanPreferencesKey("persistentShuffleAcrossQueues")
 val RememberShuffleAndRepeatKey = booleanPreferencesKey("rememberShuffleAndRepeat")
